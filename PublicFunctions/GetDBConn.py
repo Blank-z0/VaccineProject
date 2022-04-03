516311b552c7a02c4601c4900160f1d01efa5930
Blank-z0 <71818838+Blank-z0@users.noreply.github.com>
2022-04-03 22:15:39 +0800

    1.公用方法重构：PublicFunctions下ConnDB()连接mysql，DateJudgement()判断输入日期的合法性-用于Hospital和Person 2.MakeAppointment界面UI修改，显示输入日期的规则 3.Hospital中BuyVaccine方法逻辑错误修改

--- PublicFunctions/GetDBConn.py
+++ PublicFunctions/GetDBConn.py
@@ -3,10 +3,10 @@ import pymysql
 
 def ConnDB():
     connect = pymysql.connect(
-        host = '110.42.226.214',
+        host = '127.0.0.1',
         port = 3306,
         user = 'root',
-        password = 'Dbddyyzsd233!',
+        password = '*******',
         db = 'vaccine_project'
     )
     return connect
